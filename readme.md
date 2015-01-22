55b37676a84a82197fc62845f27ff072c39657d2
Rocky Madden <git@rockymadden.com>
2015-01-22 14:54:32 -0700

    Created repository.

---
+++ readme.md
@@ -0,0 +1,70 @@
+# rsyncbox <sub><sup>v0.0.0</sup></sub>
+
+Shell based Dropbox replacement with any SMB capable server (e.g. FreeNAS, Synology, QNap, Drobo).
+
+## Installation
+Make rsyncbox available in your `PATH`:
+
+```bash
+$ curl -o /usr/local/bin/rsyncbox.sh https://raw.githubusercontent.com/rockymadden/rsyncbox/master/rsyncbox.sh && chmod 0755 /usr/local/bin/rsyncbox
+```
+
+## Configuration
+Export a single global variable in `~/.bash_profile`:
+
+```bash
+$ export RSYNCBOX_REMOTE_SMB_PATH=//username@ip/share
+```
+
+## Usage
+
+```bash
+$ rsyncbox [init | status | connect | disconnect | clean | secure | push | pull]
+```
+
+You can issue mutiple subcommands at once:
+
+```bash
+$ rsyncbox connect clean secure push
+```
+
+## Subcommands
+* __init:__ Creates local and remote directories, if they do not exist
+  * `~/rsyncbox` is the local store
+  * `~/.rsyncbox` is the local configuration
+  * `${RSYNCBOX_REMOTE_SMB_PATH}/rsyncbox` is the remote store
+* __status:__ Returns the remote store status, last cleaned, last secured, last pushed, and last pulled timestamps
+* __connect:__ Connects to the remote store via `mount_smbfs`
+* __disconnect:__ Disconnects from the remote store via `unmount`
+* __clean:__ Removes all `.DS_Store` files from the local store
+* __secure:__ Secures, efficiently, the local store via `find` and `chmod`
+  * Non-executables: 600
+  * Executables: 700 
+* __push:__ Pushes delta to remote store via `rsync`
+* __pull:__ Pulls delta from remote store via `rsync`
+
+## License
+
+```
+The MIT License (MIT)
+
+Copyright (c) 2015 Rocky Madden (http://rockymadden.com/)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+```
---
+++ rsyncbox.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+function clean() {
+  touch ~/.rsyncbox/clean > /dev/null 2>&1;
+  find ~/rsyncbox -type f -name '*.DS_Store' -ls -delete
+}
+
+function connect() {
+  if [ -z "$RSYNCBOX_REMOTE_SMB_PATH" ]; then
+    echo "Remote SMB path variable not set"
+  else
+    touch ~/.rsyncbox/connect > /dev/null 2>&1;
+    mkdir /volumes/rsyncbox > /dev/null 2>&1;
+    mount_smbfs "$RSYNCBOX_REMOTE_SMB_PATH" /volumes/rsyncbox
+  fi
+}
+
+function disconnect() {
+  touch ~/.rsyncbox/disconnect > /dev/null 2>&1;
+  umount /volumes/rsyncbox > /dev/null 2>&1;
+  rmdir /volumes/rsyncbox > /dev/null 2>&1;
+}
+
+function helpful() {
+  echo "Example usage:"
+  echo "  rsyncbox [init | status | connect | disconnect | clean | secure | push | pull]"
+  echo
+  echo "Example multiple subcommand usage:"
+  echo "  rsyncbox connect clean secure push"
+}
+
+function init() {
+  mkdir ~/.rsyncbox > /dev/null 2>&1;
+  mkdir ~/rsyncbox > /dev/null 2>&1;
+
+  disconnect
+  connect
+
+  mkdir /volumes/rsyncbox/rsyncbox/ > /dev/null 2>&1;
+}
+
+function pull() {
+  touch ~/.rsyncbox/pull > /dev/null 2>&1;
+  rsync -av --inplace --delete --ignore-errors --exclude=.DS_Store /volumes/rsyncbox/rsyncbox/ ~/rsyncbox/
+}
+
+function push() {
+  touch ~/.rsyncbox/push > /dev/null 2>&1;
+  rsync -av --inplace --delete --ignore-errors --exclude=.DS_Store ~/rsyncbox/ /volumes/rsyncbox/rsyncbox/
+}
+
+function secure() {
+  touch ~/.rsyncbox/secure > /dev/null 2>&1;
+  # TODO: More extensions.
+  find ~/rsyncbox ! -perm 0700 -type d -exec chmod 0700 {} + &&
+  find ~/rsyncbox ! \( -name \*.purs -o -name \*.clj -o -name \*.coffee -o -name \*.hs -o -name \*.js -o -name \*.py -o -name \*.scala -o -name \*.sh \) ! -perm 0600 -type fl -exec chmod 0600 {} + &&
+  find ~/rsyncbox \( -name \*.purs -o -name \*.clj -o -name \*.coffee -o -name \*.hs -o -name \*.js -o -name \*.py -o -name \*.scala -o -name \*.sh \) ! -perm 0700 -type fl -exec chmod 0700 {} +
+
+  if [ -d "~/rsyncbox/dotfiles" ]; then
+    find ~/rsyncbox/dotfiles/**/.* ! -perm 0700 -type f -exec chmod 0700 {} +
+  fi
+}
+
+function status() {
+  if [ -d /volumes/rsyncbox ]; then
+    echo ' server: connected'
+  else
+    echo ' server: disconnected'
+  fi
+
+  if [ -e ~/.rsyncbox/clean ]; then
+    echo 'cleaned:' `stat -f "%Sm" -t "%Y-%m-%dT%H:%M:%S%z" ~/.rsyncbox/clean`
+  else
+    echo 'cleaned: never'
+  fi
+
+  if [ -e ~/.rsyncbox/secure ]; then
+    echo 'secured:' `stat -f "%Sm" -t "%Y-%m-%dT%H:%M:%S%z" ~/.rsyncbox/secure`
+  else
+    echo 'secured: never'
+  fi
+
+  if [ -e ~/.rsyncbox/push ]; then
+    echo ' pushed:' `stat -f "%Sm" -t "%Y-%m-%dT%H:%M:%S%z" ~/.rsyncbox/push`
+  else
+    echo ' pushed: never'
+  fi
+
+  if [ -e ~/.rsyncbox/pull ]; then
+    echo ' pulled:' `stat -f "%Sm" -t "%Y-%m-%dT%H:%M:%S%z" ~/.rsyncbox/pull`
+  else
+    echo ' pulled: never'
+  fi
+}
+
+function version() {
+  echo "v0.0.0"
+}
+
+for subcommand in "$@"
+do
+  case ${subcommand} in
+    clean) clean ;;
+    connect) connect ;;
+    disconnect) disconnect ;;
+    --help) helpful ;;
+    help) helpful ;;
+    init) init ;;
+    pull) pull ;;
+    push) push ;;
+    secure) secure ;;
+    status) status ;;
+    version) version ;;
+    --version) version ;;
+  esac
+done
